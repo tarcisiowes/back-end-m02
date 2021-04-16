2b9016fd851d14296cf0f6b60c281b300ba25352
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-04-15 21:33:12 -0300

    Add files via upload

--- back-primeiro-servidor/classe-03/index.js
+++ back-primeiro-servidor/classe-03/index.js
@@ -1,5 +1,19 @@
+  
+const express = require("express")
 
-
-
+const app = express()
 
 const jogadores = ["José", "Maria", "João", "Marcos", "Fernanda"];
+
+let i = 0;
+
+app.get("/rodada", (req, res) => {
+    if (i === jogadores.length) {
+        i = 0
+    }
+    res.send(`É a vez de ${jogadores[i]} jogar!`);
+    i++ 
+
+});
+
+app.listen(8001);

37d30e656ddfb0965895a6203e092c561925bcdd
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-04-20 20:48:14 -0300

    Add files via upload

--- back-api-rest/classe-01/index.js
+++ back-api-rest/classe-01/index.js
@@ -8,4 +8,11 @@ app.get("/convidados", (req, res) => {
     res.json(convidados);
 });
 
+app.get("/convidados/:nome", (req,res) => {
+
+    let naLista = convidados.includes(req.params.nome)
+    naLista ? res.json("mensagem: Convidado presente.") : res.json("mensagem: O convidado buscado não está presente na lista.")
+    // res.json(naLista);
+});
+
 app.listen(8000);

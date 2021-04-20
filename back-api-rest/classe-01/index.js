68f8001a1b0def7594c29c88d97f1688dbed88a2
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-04-20 20:28:02 -0300

    Add files via upload

--- back-api-rest/classe-01/index.js
+++ back-api-rest/classe-01/index.js
@@ -1,1 +1,11 @@
-.
+const express = require("express");
+
+const app = express()
+
+const convidados = ["Carlos", "Amanda", "Fernanda", "Juliana", "Lucas", "Roberto"];
+
+app.get("/convidados", (req, res) => {
+    res.json(convidados);
+});
+
+app.listen(8000);

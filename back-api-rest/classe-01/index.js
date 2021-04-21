02886d3c8a054dd52b55d70939e5f6da4b153efe
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-04-20 21:48:22 -0300

    tarwes back-m2-a3-e1c

--- back-api-rest/classe-01/index.js
+++ back-api-rest/classe-01/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+// const bodyParser = require("body-parser");
 
 const app = express()
+app.use(express.json());
 
 const convidados = ["Carlos", "Amanda", "Fernanda", "Juliana", "Lucas", "Roberto"];
 
@@ -8,11 +10,29 @@ app.get("/convidados", (req, res) => {
     
     if(req.query.nome) {
         let naLista = convidados.includes(req.query.nome)
-        naLista ? res.json("mensagem: Convidado presente.") : res.json("mensagem: O convidado buscado não está presente na lista.")
+        naLista ? res.json({ mensagem: "Convidado presente" }) : res.json({ mensagem: "O convidado buscado não está presente na lista" })
     } else {
         res.json(convidados);
     }
        
 });
+
+//POST
+
+app.post("/convidados", (req, res) => {
+    
+    if(convidados.includes(req.body.nome)) {
+        res.json({
+            mensagem: "O nome do convidado a ser adicionado já existe na lista. Caso queria adicionar outro convidado de mesmo nome, favor fornecer o sobrenome também."
+        });
+    } else {
+        
+        convidados.push(req.body.nome)
+        res.json(req.body)
+        
+    }
+
+});
+
 
 app.listen(8000);

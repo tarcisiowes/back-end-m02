fdc1bbfbb2ea36e590d5014bb4f268b531efc537
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-04-20 21:09:29 -0300

    tarwes back-m2-a3-e1b

--- back-api-rest/classe-01/index.js
+++ back-api-rest/classe-01/index.js
@@ -5,14 +5,14 @@ const app = express()
 const convidados = ["Carlos", "Amanda", "Fernanda", "Juliana", "Lucas", "Roberto"];
 
 app.get("/convidados", (req, res) => {
-    res.json(convidados);
-});
-
-app.get("/convidados/:nome", (req,res) => {
-
-    let naLista = convidados.includes(req.params.nome)
-    naLista ? res.json("mensagem: Convidado presente.") : res.json("mensagem: O convidado buscado não está presente na lista.")
-    // res.json(naLista);
+    
+    if(req.query.nome) {
+        let naLista = convidados.includes(req.query.nome)
+        naLista ? res.json("mensagem: Convidado presente.") : res.json("mensagem: O convidado buscado não está presente na lista.")
+    } else {
+        res.json(convidados);
+    }
+       
 });
 
 app.listen(8000);

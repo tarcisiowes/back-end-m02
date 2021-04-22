9c4ce76faec70caa7d13364f9622ba101abf5a41
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-04-22 20:18:38 -0300

    b

--- back-rotas-e-controladores/classe-01/index.js
+++ back-rotas-e-controladores/classe-01/index.js
@@ -2,26 +2,7 @@ const express = require("express")
 
 const app = express()
 
-const imoveis = [
-    {
-      id: 1,
-      logradouro: "Av. Getúlio Vargas",
-      numero: 1458,
-      complemento: "Térreo",
-      bairro: "Funcionários",
-      cidade: "Belo Horizonte",
-      cep: "30112-021"
-    },
-    {
-      id: 2,
-      logradouro: "Av. Paulista",
-      numero: 1009,
-      complemento: "16º andar",
-      bairro: "Cerqueira César",
-      cidade: "São Paulo",
-      cep: "01310-100"
-    }
-  ];
+const { imoveis } = require("./dados/imoveis");
 
 
 app.get("/imoveis", (req, res) => {
@@ -34,5 +15,6 @@ app.get("/imoveis/:numero", (req, res) => {
 
     res.json(imoveis[req.params.numero-1])
 })
+
 
 app.listen(8000);

1c28609d9728a1ceae6abfc60114423d4b3eed23
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-04-24 18:40:15 -0300

    Add files via upload

--- back-primeiro-servidor/casa-06/index.js
+++ back-primeiro-servidor/casa-06/index.js
@@ -3,36 +3,60 @@ const express = require("express");
 
 const app = express()
 
-let segundos = 0;
-let minutos = 0;
+let segundos = 0
+let minutos = 0
+let comecou = false
+let iniciado = false
 
-app.get("/iniciar", (req, res) => {
+function start() {
 
-    
+    comecou = true
+    if (!iniciado) {
 
-    function seg() {
+        setInterval(() => {
 
+            if (comecou) {
 
+                if (segundos == 59) {
+                    segundos = 0
+                    minutos++
+                } else {
+                    segundos++
+                }
+            }
 
-        if (segundos == 60) {
-        segundos = 0;
-        minutos++;
-        } else {            
-            segundos++ 
-        }
-        
+        }, 1000)
+
+        iniciado = true
     }
 
-res.send(`Tempo atual do cronômetro: ${minutos} minutos e ${segundos} segundos`);
+}
 
-     
-const intervalId = setInterval(seg,1000) 
-    
-    
-    
+app.get("/", (req, res) => {
+    res.send(`Tempo atual do cronômetro: ${minutos.toString().padStart(2, "0")} minutos e ${segundos.toString().padStart(2, "0")} segundos`)
 });
 
-// clearInterval()
+app.get("/iniciar", (req, res) => {
+    start();
+    res.send("Cronômetro iniciado!")
+});
+
+app.get("/pausar", (req, res) => {
+    comecou = false;
+    res.send("Cronômetro pausado!");
+});
+
+app.get("/continuar", (req, res) => {
+    comecou = true;
+    res.send("Cronômetro continuando!")
+});
+
+app.get("/zerar", (req, res) => {
+    minutos = 0;
+    segundos = 0;
+    res.send("Cronômetro zerado!")
+});
+
+app.listen(8000)
 
 
-app.listen(8000);
